79c5fe6b47e43c70a0700899a9b28b65803f48a3
unknown <you@example.com>
2024-06-30 12:29:52 +0530

    Initial Code Setup Done

---
+++ Cab-Deriver/src/main/java/com/cabderiver/Cab/Deriver/CabDeriverApplication.java
@@ -0,0 +1,13 @@
+package com.cabderiver.Cab.Deriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabDeriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabDeriverApplication.class, args);
+	}
+
+}
---
+++ Cab-Deriver/src/test/java/com/cabderiver/Cab/Deriver/CabDeriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabderiver.Cab.Deriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabDeriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
